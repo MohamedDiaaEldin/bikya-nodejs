f536ddc3c0ccb490ab09a0040cbd4e272176b359
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-05-18 15:58:51 +0300

    Feat : Add Not Found Response Handler - Return 404  and "Not Found" Message

--- src/utils/responseHandler.js
+++ src/utils/responseHandler.js
@@ -23,11 +23,17 @@ const sendUnAuthorized = (res, message = "Unauthorized") => {
   return res.status(401).json({ message });
 };
 
+// Function to send an unauthorized response
+const sendNotFoundResponse = (res, message = "Not Found") => {
+  return res.status(404).json({ message });
+};
+
 // Exporting the response functions for external use
 module.exports = {
   sendSuccessResponse, // Sends a success response
   sendErrorResponse, // Sends an error response
   sendConflictResponse, // Sends a conflict response
   sendBadRequestResponse, // Sends a bad request response
-  sendUnAuthorized // Sends an unauthorized response
+  sendUnAuthorized , // Sends an unauthorized response
+  sendNotFoundResponse // Sends not found response
 };
