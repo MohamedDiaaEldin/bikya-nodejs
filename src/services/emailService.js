5335279aca908c049d64ea30064ee0a94a69ca9b
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-05-19 09:31:14 +0300

    Feat : Add Async Send Password Confirmation Email Functionality - Utilize nodemailer and sendMail Functionalities - Send HTML and CSS Template with Password Change Confirmation

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -42,11 +42,83 @@ const sendOTPMail = async (to, otp) => {
 
 
 
-
+const sendPasswordChangeConfirmationEmail = async(email, username)=> { 
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Password Change Confirmation</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f4f4f4;
+        margin: 0;
+        padding: 0;
+      }
+      .container {
+        width: 100%;
+        max-width: 600px;
+        margin: 0 auto;
+        padding: 20px;
+        background-color: #ffffff;
+        border: 1px solid #e0e0e0;
+        border-radius: 5px;
+      }
+      .header {
+        text-align: center;
+        padding: 10px 0;
+        border-bottom: 1px solid #e0e0e0;
+      }
+      .header img {
+        width: 50px;
+      }
+      .content {
+        padding: 20px;
+      }
+      .content h1 {
+        color: #333333;
+        font-size: 24px;
+        margin-bottom: 20px;
+      }
+      .content p {
+        color: #666666;
+        font-size: 16px;
+        line-height: 1.5;
+        margin-bottom: 20px;
+      }
+      .footer {
+        text-align: center;
+        padding: 10px 0;
+        border-top: 1px solid #e0e0e0;
+        color: #999999;
+        font-size: 12px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">    
+      <div class="content">
+        <h1>Password Change Confirmation</h1>
+        <p>Dear ${username},</p>
+        <p>We wanted to let you know that your password has been changed successfully. If you did not make this change or if you believe an unauthorized person has accessed your account, please reset your password immediately and contact our support team.</p>
+        <p>If you have any questions or need further assistance, please do not hesitate to contact us.</p>
+        <p>Thank you for using our service!</p>
+        <p>Best regards,<br>Bikya</p>
+      </div>
+      <div class="footer">
+        &copy; 2024 Bikya. All rights reserved.
+      </div>
+    </div>
+  </body>
+  </html>
+  `
+  await sendMail(email,'Password Change Confirmation', '', html)
+}
 
 // Export the function to be used in other modules
 module.exports = {
   sendOTPMail,
+  sendPasswordChangeConfirmationEmail
 };
 
 
